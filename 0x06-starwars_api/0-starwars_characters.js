8654c1f1bf3e9f160e90440a1c68a79352da2155
Eustus <Mwirigieustus7@gmail.com>
2024-04-18 02:38:18 -0700

    chages

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,19 +7,28 @@ const options = {
 };
 
 request(options, function (error, response, body) {
-  if (!error) {
+  if (!error && response.statusCode === 200) {
     const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    if (characters && characters.length > 0) {
+      printCharacters(characters, 0);
+    } else {
+      console.log('No characters found for this movie.');
+    }
+  } else {
+    console.error('Error fetching movie data:', error);
   }
 });
 
 function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
-    if (!error) {
+    if (!error && response.statusCode === 200) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
         printCharacters(characters, index + 1);
       }
+    } else {
+      console.error('Error fetching character data:', error);
     }
   });
 }
+
